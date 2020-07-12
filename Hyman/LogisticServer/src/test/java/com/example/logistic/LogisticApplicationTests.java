f8c3e639bc3060c6b802cd607efdc9c789e5bdc3
chenhy <chenhy@oocl.com>
2020-07-13 01:30:33 +0800

    initial

---
+++ Hyman/LogisticServer/src/test/java/com/example/logistic/LogisticApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.logistic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogisticApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
